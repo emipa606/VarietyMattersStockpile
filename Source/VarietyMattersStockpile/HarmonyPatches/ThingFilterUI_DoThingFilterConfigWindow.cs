300b1beecece56ffd2ca76afb938cf42e5485889
stewbyte <xgenyofficial@gmail.com>
2025-07-14 18:31:44 +0200

    Fix checkbox bindings in stockpile UI
    This fixes broken checkbox behavior in the stockpile UI for the mod under RimWorld 1.6.

--- Source/VarietyMattersStockpile/HarmonyPatches/ThingFilterUI_DoThingFilterConfigWindow.cs
+++ Source/VarietyMattersStockpile/HarmonyPatches/ThingFilterUI_DoThingFilterConfigWindow.cs
@@ -13,9 +13,7 @@ public class ThingFilterUI_DoThingFilterConfigWindow
     {
         var tab = Mod_VarietyStockpile.currentTab;
         if (tab == null)
-        {
             return;
-        }
 
         rect.yMin += Mod_VarietyStockpile.minY;
     }
@@ -24,18 +22,14 @@ public class ThingFilterUI_DoThingFilterConfigWindow
     {
         var tab = Mod_VarietyStockpile.currentTab;
         if (tab == null)
-        {
             return;
-        }
 
         var storeSettingsParent = (IStoreSettingsParent)typeof(ITab_Storage)
             .GetProperty("SelStoreSettingsParent", BindingFlags.NonPublic | BindingFlags.Instance)
             ?.GetGetMethod(true).Invoke(tab, []);
         var settings = storeSettingsParent?.GetStoreSettings();
         if (settings?.owner is not ISlotGroupParent slotGroupParent)
-        {
             return;
-        }
 
         var limitSettings = StorageLimits.GetLimitSettings(settings);
 
@@ -48,112 +42,88 @@ public class ThingFilterUI_DoThingFilterConfigWindow
         if (limitDuplicates)
         {
             if (Mod_VarietyStockpile.oldSettings != settings)
-            {
                 Mod_VarietyStockpile.dupBuffer = dupLimit.ToString();
-            }
 
             Widgets.TextFieldNumeric(
-                new Rect(rect.xMin + (rect.width / 2) - 40f, rect.yMin - Mod_VarietyStockpile.minY,
-                    (rect.width / 2) - 115f, 20f),
-                ref dupLimit, ref Mod_VarietyStockpile.dupBuffer, 1, Mod_VarietyStockpile.max);
+                new Rect(rect.xMin + (rect.width / 2) - 40f, rect.yMin - Mod_VarietyStockpile.minY, (rect.width / 2) - 115f, 20f),
+                ref dupLimit,
+                ref Mod_VarietyStockpile.dupBuffer,
+                1,
+                Mod_VarietyStockpile.max);
+
+            if (dupLimit < 1)
+                dupLimit = 1;
         }
         else
         {
             dupLimit = -1;
         }
+        limitSettings.DupStackLimit = dupLimit;
 
         //Stack Limit
         var sizeLimit = limitSettings.StackSizeLimit;
         var hasLimit = sizeLimit != -1;
         Widgets.CheckboxLabeled(
-            new Rect(rect.xMin + (rect.width / 2) - 5f, rect.yMin - Mod_VarietyStockpile.minY, (rect.width / 2) - 45f,
-                20f),
+            new Rect(rect.xMin + (rect.width / 2) - 5f, rect.yMin - Mod_VarietyStockpile.minY, (rect.width / 2) - 45f, 20f),
             "VMS.StackSize".Translate(),
             ref hasLimit);
         if (hasLimit)
         {
             if (Mod_VarietyStockpile.oldSettings != settings)
-            {
                 Mod_VarietyStockpile.sizeBuffer = sizeLimit.ToString();
-            }
 
             Widgets.TextFieldNumeric(
-                new Rect(rect.xMin + (rect.width / 2) + 95f, rect.yMin - Mod_VarietyStockpile.minY,
-                    (rect.width / 2) - 105f, 20f),
-                ref sizeLimit, ref Mod_VarietyStockpile.sizeBuffer, 1, Mod_VarietyStockpile.max);
+                new Rect(rect.xMin + (rect.width / 2) + 95f, rect.yMin - Mod_VarietyStockpile.minY, (rect.width / 2) - 105f, 20f),
+                ref sizeLimit,
+                ref Mod_VarietyStockpile.sizeBuffer,
+                1,
+                Mod_VarietyStockpile.max);
+
+            if (sizeLimit < 1)
+                sizeLimit = 1;
         }
         else
         {
             sizeLimit = -1;
         }
+        limitSettings.StackSizeLimit = sizeLimit;
 
-        //Refill
-        var cellFillPercentage = limitSettings.CellFillPercentage * 100;
-        var filledCells = 0;
-        var numCells = 0;
-        var foundBuilding = false;
-
-        foreach (var intVec3 in slotGroupParent.AllSlotCells())
+        //Refill & Fill Now
+        var cellFillPct = limitSettings.CellFillPercentage * 100f;
+        int filled = 0, total = 0;
+        bool found = false;
+        foreach (var c in slotGroupParent.AllSlotCells())
         {
-            var thingList = intVec3.GetThingList(slotGroupParent.Map);
-            filledCells += thingList.Count(t => t.def.EverStorable(false));
-
-            foreach (var thing in thingList)
+            var list = c.GetThingList(slotGroupParent.Map);
+            filled += list.Count(t => t.def.EverStorable(false));
+            foreach (var t in list)
             {
-                if (thing is not Building building)
-                {
-                    continue;
-                }
-
-                foundBuilding = true;
-                numCells += building.MaxItemsInCell;
+                if (t is not Building b) continue;
+                found = true;
+                total += b.MaxItemsInCell;
                 break;
             }
         }
+        if (!found)
+            total = slotGroupParent.AllSlotCellsList().Count;
 
-        if (!foundBuilding)
+        int startAt = Mathf.CeilToInt((100 - cellFillPct) / 100 * total);
+        string label = startAt == 0 ? "VMS.FullyStocked".Translate() :
+                        startAt == 1 ? "VMS.StartAtOne".Translate() :
+                        startAt == total ? "VMS.RefillWhenEmpty".Translate() : "VMS.RefillWhenAmount".Translate(startAt.ToString("N0"));
+
+        cellFillPct = Widgets.HorizontalSlider(new Rect(0f, rect.yMin - Mod_VarietyStockpile.minY - 40f, rect.width, 36f), cellFillPct, 0f, 100f, false, label);
+
+        if (cellFillPct < 100f && limitSettings.CellsFilled != total)
         {
-            numCells = slotGroupParent.AllSlotCellsList().Count;
-        }
-
-        var numCellsStart = Mathf.CeilToInt((100 - cellFillPercentage) / 100 * numCells);
-        var startFilling = numCellsStart == 0 || numCells - filledCells >= numCellsStart;
-        string label;
-
-        switch (numCellsStart)
-        {
-            case 0:
-                label = "VMS.FullyStocked".Translate();
-                break;
-            case 1:
-                label = "VMS.StartAtOne".Translate();
-                break;
-            default:
-                label = numCellsStart == numCells
-                    ? "VMS.RefillWhenEmpty".Translate()
-                    : "VMS.RefillWhenAmount".Translate(numCellsStart.ToString("N0"));
-
-                break;
-        }
-
-        cellFillPercentage = Widgets.HorizontalSlider(
-            new Rect(0f, rect.yMin - Mod_VarietyStockpile.minY - 40, rect.width, 36f),
-            cellFillPercentage, 0f, 100f, false, label);
-        if (cellFillPercentage < 100 && limitSettings.CellsFilled != numCells)
-        {
-            Widgets.CheckboxLabeled(
-                new Rect(rect.xMin + (rect.width * 0.6f), rect.yMin - Mod_VarietyStockpile.minY - 70, rect.width * .30f,
-                    30f),
+            Widgets.CheckboxLabeled(new Rect(rect.xMin + (rect.width * 0.6f), rect.yMin - Mod_VarietyStockpile.minY - 70f, rect.width * 0.30f, 30f),
                 "VMS.FillNow".Translate(),
-                ref startFilling);
+                ref limitSettings.NeedsFilled);
         }
 
         //Update Settings
         Mod_VarietyStockpile.oldSettings = settings;
-        limitSettings.DupStackLimit = dupLimit;
-        limitSettings.StackSizeLimit = sizeLimit;
-        limitSettings.CellFillPercentage = cellFillPercentage / 100f;
-        limitSettings.NeedsFilled = startFilling;
+        limitSettings.CellFillPercentage = cellFillPct / 100f;
         StorageLimits.SetLimitSettings(settings, limitSettings);
     }
 }
